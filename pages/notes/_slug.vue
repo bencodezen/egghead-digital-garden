f69bd2d182027ba4da18fc5f7c530da8be360bf9
Ben Hong <ben@bencodezen.io>
2021-10-14 22:23:18 -0400

    Feature: add filter and such

---
+++ pages/notes/_slug.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  async asyncData({ $content, route }) {
+    const note = await $content(`notes/${route.params.slug}`).fetch();
+
+    return {
+      note
+    };
+  }
+};
+</script>
+
+<template>
+  <main>
+    <h1>{{ note.title }}</h1>
+    <nuxt-content :document="note" />
+  </main>
+</template>
--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -1,10 +1,25 @@
 <script>
 export default {
+  async asyncData({ $content }) {
+    const notes = await $content("notes")
+      .sortBy("publishOn", "desc")
+      .fetch();
+
+    return {
+      notes
+    };
+  },
   data: () => ({
-    notes: []
+    selectedTag: ""
   }),
-  async created() {
-    this.notes = await this.$content("notes").fetch();
+  computed: {
+    displayedNotes() {
+      if (this.selectedTag) {
+        return this.notes.filter(note => note.tags.includes(this.selectedTag));
+      } else {
+        return this.notes;
+      }
+    }
   }
 };
 </script>
@@ -12,12 +27,22 @@ export default {
 <template>
   <main>
     <h1>My Notes</h1>
+    <p v-show="selectedTag">
+      Filtered by: {{ selectedTag }}
+      <button @click="selectedTag = ''">Clear</button>
+    </p>
     <ul>
-      <li v-for="note in notes" :key="note.slug + note.createdAt">
-        <h2>{{ note.title }}</h2>
+      <li v-for="note in displayedNotes" :key="note.slug + note.createdAt">
+        <h2>
+          <nuxt-link :to="`/notes/${note.slug}`">{{ note.title }}</nuxt-link>
+        </h2>
         <p>Publish on: {{ new Date(note.publishOn) }}</p>
         <ul>
-          <li v-for="tag in note.tags" :key="note.slug + tag">
+          <li
+            v-for="tag in note.tags"
+            :key="note.slug + tag"
+            @click="selectedTag = tag"
+          >
             {{ tag }}
           </li>
         </ul>
