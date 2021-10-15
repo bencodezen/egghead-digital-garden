5fc2c0f34d183fedc57de6d19cb83ee31204d666
Ben Hong <ben@bencodezen.io>
2021-10-14 21:40:19 -0400

    feature: add note fetch

---
+++ pages/notes/index.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data: () => ({
+    notes: []
+  }),
+  async created() {
+    this.notes = await this.$content("notes").fetch();
+  }
+};
+</script>
+
+<template>
+  <main>
+    <h1>My Notes</h1>
+    <ul>
+      <li v-for="note in notes" :key="note.slug + note.createdAt">
+        <h2>{{ note.slug }}</h2>
+      </li>
+    </ul>
+  </main>
+</template>
